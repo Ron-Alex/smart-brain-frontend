100cfb23dd2e3c9f8a35ea27230ccacb652b8d3a
Ron Alex <ronmathewalx@gmail.com>
2025-02-10 23:15:19 -0600

    Changing clairfai endpoint to backend

--- src/App.js
+++ src/App.js
@@ -68,71 +68,26 @@ function App() {
 
   const onBtnSubmit = () => {
     setURL(input); // Set the image URL
-  
-    const PAT = 'b7f5f05974764529ab113565cd90b500';
-    const USER_ID = 'mljx0bi1jwgg';
-    const APP_ID = 'my-first-application-klq8aa';
-    const MODEL_ID = 'face-detection';
-    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';
-  
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: USER_ID,
-        app_id: APP_ID,
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: input,
-            },
-          },
-        },
-      ],
-    });
-  
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        Authorization: 'Key ' + PAT,
-      },
-      body: raw,
-    };
-  
-    fetch(`https://api.clarifai.com/v2/models/${MODEL_ID}/versions/${MODEL_VERSION_ID}/outputs`,requestOptions)
-      .then((response) => response.json())
-      .then((result) => {
-        if(result) {
-          fetch("https://quiet-shelf-08190-ced9ba4506b3.herokuapp.com/image", {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              id: user.id
-            })
-          })
-          .then(response => response.json())
-          .then(count => {
-            setUser(prevUser => ({...prevUser, entries: count}))
-          })
-        }
-        const regions = result.outputs[0]?.data?.regions;
-        if (!regions) throw new Error('No regions found in the response.');
-  
-        const newBoxes = regions.map((region) => {
-          const boundingBox = region.region_info.bounding_box;
-  
-          return calculateFaceLocation({
-            topRow: boundingBox.top_row,
-            leftCol: boundingBox.left_col,
-            bottomRow: boundingBox.bottom_row,
-            rightCol: boundingBox.right_col,
-          });
-        });
-  
-        setBox(newBoxes); // Set state with the new bounding boxes (replacing old boxes)
+
+    fetch("https://quiet-shelf-08190-ced9ba4506b3.herokuapp.com/clarifai"), {
+      method: 'get',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        imageURL: URL
       })
-      .catch((error) => console.log('error', error));
+    }
+    
+    fetch("https://quiet-shelf-08190-ced9ba4506b3.herokuapp.com/image", {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        id: user.id
+      })
+    })
+    .then(response => response.json())
+    .then(count => {
+      setUser(prevUser => ({...prevUser, entries: count}))
+    })
   };
 
   const onRouteChange = (route) => {
